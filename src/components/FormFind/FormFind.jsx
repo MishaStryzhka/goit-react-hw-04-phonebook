97d155540839c5d4030c9ce4512cc60111f36954
MishaStryzhka <misha.strizhcko@gmail.com>
2023-05-02 13:16:47 +0200

    Adds refactoring the code into hooks

--- src/components/FormFind/FormFind.jsx
+++ src/components/FormFind/FormFind.jsx
@@ -1,27 +1,21 @@
-import { Component } from "react";
 import PropTypes from 'prop-types';
 import css from "./FormFind.module.css"
 
 // import css from './FormFind.module.css';
 
-
-
-class FormFind extends Component {
-
-    render() {
-        return <form className={css.form} >
-            <label className={css.label}>Find contacts by name</label>
-            <input
-                type="text"
-                name="filter"
-                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-                value={this.value}
-                onChange={this.props.handleChange}
-            />
-        </form>
-    }
+const FormFind = ({ value, handleChange }) => {
+    return <form className={css.form} >
+        <label className={css.label}>Find contacts by name</label>
+        <input
+            type="text"
+            name="filter"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={value}
+            onChange={handleChange}
+        />
+    </form>
 }
 
 FormFind.propTypes = {
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,58 +1,48 @@
-import { Component } from "react";
+
 import { nanoid } from "nanoid";
 
 import FormContacts from "components/FormContacts/FormContacts";
 import FormFind from "components/FormFind/FormFind";
 import css from './Phonebook.module.css';
+import { useState, useEffect } from "react";
 
 const KEYCONTACTS = "contacts";
 
-class Phonebook extends Component {
-    state = {
-        contacts: [],
-        filter: ''
-    }
+const Phonebook = () => {
+    const [contacts, setContacts] = useState([]);
+    const [filter, setFilter] = useState("")
 
-    componentDidMount = () => {
+
+    useEffect(() => {
         if (localStorage.getItem(KEYCONTACTS)) {
-            this.setState({ contacts: JSON.parse(localStorage.getItem(KEYCONTACTS)) })
+            setContacts(JSON.parse(localStorage.getItem(KEYCONTACTS)));
         }
-    }
+    }, [])
 
+    useEffect(() => {
+        localStorage.setItem(KEYCONTACTS, JSON.stringify(contacts));
+    }, [contacts]);
 
-    componentDidUpdate = (_, prevState) => {
-        if (this.state.contacts) {
-            if (prevState.contacts.length !== this.state.contacts.length) {
-                console.log(this.state.contacts);
-                localStorage.setItem(KEYCONTACTS, JSON.stringify(this.state.contacts));
-            }
-        }
-    }
-
-    onSubmit = (user) => {
-        if (this.state.contacts && this.state.contacts.find(contact => {
+    const onSubmit = (user) => {
+        if (contacts && contacts.find(contact => {
             const normalizeUser = user.name.toLowerCase();
             return contact.name.toLowerCase() === normalizeUser
         })) { alert("Даний контакт вже є в телефонній") }
         else {
             const newUser = { id: nanoid(), ...user }
-            return this.state.contacts
-                ? this.setState((prefState) => ({ ...prefState, contacts: [...prefState.contacts, newUser] }))
-                : this.setState({ contacts: [newUser] })
+            setContacts((pref) => [...pref, newUser])
         }
     }
 
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
+    const handleChange = e => {
+        setFilter(e.target.value)
     };
 
-    handleRemove = e => {
-        this.setState({ contacts: this.state.contacts.filter(value => value.id !== e.currentTarget.parentNode.id) })
-    }
+    const handleRemove = e => {
+        setContacts(contacts.filter(value => value.id !== e.currentTarget.parentNode.id))
+    };
 
-    getVizibleContacts = () => {
-        const { contacts, filter } = this.state;
-
+    const getVizibleContacts = () => {
         const normalizedFilter = filter.toLowerCase();
 
         if (contacts) {
@@ -61,31 +51,30 @@ class Phonebook extends Component {
 
         return []
 
-    }
-
-    render() {
-        return (
-            <>
-                <h1 className={css.title}>Phonebook</h1>
-
-                <FormContacts onSubmit={this.onSubmit} name />
-
-                <h2 className={css.title}>Contacts</h2>
-
-                <FormFind handleChange={this.handleChange} value={this.state.filter} />
-
-                <ul className={css.contactsList}>
-                    {this.getVizibleContacts().map((contact) =>
-                        <li className={css.item} key={contact.id} id={contact.id}>
-                            {contact.name} {contact.number}
-                            <button type="button" className="btn-close" aria-label="Close" onClick={this.handleRemove}></button>
-                        </li>
-                    )}
-                </ul>
-            </>
-        );
     };
 
-}
+
+    return (
+        <>
+            <h1 className={css.title}>Phonebook</h1>
+
+            <FormContacts onSubmit={onSubmit} name />
+
+            <h2 className={css.title}>Contacts</h2>
+
+            <FormFind handleChange={handleChange} value={filter} />
+
+            <ul className={css.contactsList}>
+                {getVizibleContacts().map((contact) =>
+                    <li className={css.item} key={contact.id} id={contact.id}>
+                        {contact.name} {contact.number}
+                        <button type="button" className="btn-close" aria-label="Close" onClick={handleRemove}></button>
+                    </li>
+                )}
+            </ul>
+        </>
+    );
+
+};
 
 export default Phonebook;
